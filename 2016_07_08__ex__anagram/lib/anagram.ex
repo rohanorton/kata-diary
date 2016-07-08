aa7afc7609363e9d924cdbc6c0ef8bd329003f67
Rohan Orton <rohan.orton@gmail.com>
2016-07-08 07:02:17 +0100

    Define and test is_anagram function

--- 2016_07_08__ex__anagram/lib/anagram.ex
+++ 2016_07_08__ex__anagram/lib/anagram.ex
@@ -1,2 +1,14 @@
 defmodule Anagram do
+
+  def is_anagram(string_1, string_2) do
+    normalise_string(string_1) == normalise_string(string_2)
+  end
+
+  defp normalise_string(string) do
+    String.downcase(string)
+    |> String.replace(~r/\s/, "")
+    |> String.to_char_list
+    |> Enum.sort
+  end
+
 end
--- 2016_07_08__ex__anagram/test/anagram_test.exs
+++ 2016_07_08__ex__anagram/test/anagram_test.exs
@@ -2,7 +2,24 @@ defmodule AnagramTest do
   use ExUnit.Case
   doctest Anagram
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "is_anagram true if word is same" do
+    assert Anagram.is_anagram("foo", "foo")
   end
+
+  test "is_anagram false if word contains different letters" do
+    refute Anagram.is_anagram("foo", "bar")
+  end
+
+  test "is_anagram true is contains same letters" do
+    assert Anagram.is_anagram("foo", "ofo")
+  end
+
+  test "is_anagram is case insensitive" do
+    assert Anagram.is_anagram("FOO", "ofo")
+  end
+
+  test "is_anagram ignores whitespace" do
+    assert Anagram.is_anagram("foo", "o  o   f")
+  end
+
 end
