4920140dc817e8ae9b3c3322a218f36e3ba0696a
Rohan Orton <rohan.orton@gmail.com>
2016-07-13 22:51:24 +0100

    Test passes

--- 2016_07_11__ex__bowling_game/lib/bowling_game.ex
+++ 2016_07_11__ex__bowling_game/lib/bowling_game.ex
@@ -12,6 +12,7 @@ defmodule BowlingGame do
   defp score([], score), do: score
   defp score(game, score) do
     case game do
+      [10, 10, 10 ] -> score([], 30 +  score)
       [10, b, c | rest ] -> score([b, c | rest], 10 + b + c + score)
       [a, b, c | rest ] when a + b == 10 -> score([c | rest], 10 + c + score)
       [a, b | rest ] -> score(rest, a + b + score)
